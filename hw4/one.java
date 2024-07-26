2c5436df23dc1bfa49c9566f2e91753735245544
katekhuss <khussainova.ye@gmail.com>
2024-07-26 18:44:30 +0500

    Update one.java

--- hw4/one.java
+++ hw4/one.java
@@ -4,25 +4,40 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.HashMap;
 
+// Изучить API ArrayList, LinkedList, HashMap. 
+// Создать коллекции, добавить в начало, конец, середину элементы, удалить элемент, список элементов.
+
 public class one {
     public static void main(String[] args) {
+        
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("A");
         arrayList.add(0, "B");
         arrayList.add(1, "C");
         arrayList.remove("B");
+        for (String element : arrayList) {
+            System.out.println(element);
+        }
+        System.out.println();
 
         LinkedList<int> linkedList = new LinkedList<>();
         linkedList.add(1);
         linkedList.addFirst(2);
         linkedList.add(1, 3);
         linkedList.remove(2);
+        for (Integer element : linkedList) {
+            System.out.println(element);
+        }
+        System.out.println();
 
         HashMap<Integer, String> hashMap = new HashMap<>();
         hashMap.put(1, "A");
         hashMap.put(2, "B");
         hashMap.put(1, "C");
         hashMap.remove(2);
+        for (HashMap.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + entry.getValue());
+        }
 
     }
 }
