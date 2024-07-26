a776bb2a9cf6ab84d2ea94f313ef9a1663f5398e
katekhuss <khussainova.ye@gmail.com>
2024-07-26 18:52:58 +0500

    Update Animal.java

--- hw2/one/animals/Animal.java
+++ hw2/one/animals/Animal.java
@@ -12,14 +12,6 @@ public abstract class Animal {
     public abstract void voice();
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Animal animal = (Animal) o;
-        return Objects.equals(name, animal.name);
-    }
-
-    @Override
     public int hashCode() {
         return Objects.hash(name);
     }
